e193e172f425d94fce913fe3ab3031256826b8b6
natorsc <natorsc@gmail.com>
2019-12-29 22:36:14 -0300

    Expondo um método Python para o QML. Exemplos de como alterar o estilo básico do APP (style) via `os` e `sys.argv`.

--- src/main_window/qml/qml_engine.py
+++ src/main_window/qml/qml_engine.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
-"""."""
-from PySide2.QtCore import QObject
+"""
+URLs:
+
+- https://doc.qt.io/qt-5/qtquickcontrols2-universal.html
+- https://doc.qt.io/qt-5/qtquickcontrols2-styles.html
+
+- Default Style
+- Fusion Style
+- Imagine Style
+- Material Style
+- Universal Style
+
+Variáveis de ambiente:
+
+- QT_QUICK_CONTROLS_UNIVERSAL_THEME: Tema que será utilizado (Ex: 'Dark').
+- QT_QUICK_CONTROLS_UNIVERSAL_ACCENT: Cor que será utilizada nos detalhes (Ex: 'Violet'.
+- QT_QUICK_CONTROLS_UNIVERSAL_FOREGROUND: (Ex: 'Brown').
+- QT_QUICK_CONTROLS_UNIVERSAL_BACKGROUND:
+
+Constantes:
+
+- Universal.Light: Light theme (default)
+- Universal.Dark: Dark theme
+- Universal.System: System theme
+"""
+import os
+
+from PySide2.QtCore import QObject, Slot
 from PySide2.QtGui import QGuiApplication, QIcon
 from PySide2.QtQml import QQmlApplicationEngine
 
 
-class MainWindow:
-    def __init__(self, win):
+class MainWindow(QObject):
+    def __init__(self, window):
+        super().__init__()
 
-        self.labe1 = win.findChild(QObject, 'label')
-        self.text_field = win.findChild(QObject, 'textfield')
-        self.button = win.findChild(QObject, 'button')
-        self.button.clicked.connect(self.change_label)
+        self.labe1 = window.findChild(QObject, 'label')
+        self.text_field = window.findChild(QObject, 'textfield')
+        button = window.findChild(QObject, 'button')
+        button.clicked.connect(self.change_label)
 
     def change_label(self):
         if self.text_field.property('text'):
@@ -19,16 +46,41 @@ class MainWindow:
         else:
             self.labe1.setProperty('text', 'Digite algo no campo de texto :)')
 
+    @Slot(str)
+    def _on_button_clicked(self, text):
+        print('Botão clicado (Python)')
+        if text:
+            self.labe1.setProperty('text', text)
+        else:
+            self.labe1.setProperty('text', 'Digite algo no campo de texto :)')
+
 
 if __name__ == "__main__":
     import sys
 
+    os.environ['QT_QUICK_CONTROLS_STYLE'] = 'Material'
+
+    # sys.argv += ['--style', 'Default']
+    # sys.argv += ['--style', 'Fusion']
+    # sys.argv += ['--style', 'Imagine']
+    # sys.argv += ['--style', 'Material']
+    # sys.argv += ['--style', 'Universal']
+
     app = QGuiApplication(sys.argv)
     app.setWindowIcon(QIcon('../../_icons/icon.png'))
+
     engine = QQmlApplicationEngine()
     engine.load('./ui/interface_engine.qml')
+
     if not engine.rootObjects():
         sys.exit(-1)
-    win = engine.rootObjects()[0]
-    ui = MainWindow(win=win)
+
+    window = engine.rootObjects()[0]
+    mainwindow = MainWindow(window=window)
+
+    # Expondo o Python para o QML.
+    # Caso os métodos sejam criados no QML (@SLOT()).
+    context = engine.rootContext()
+    context.setContextProperty('MainWindow', mainwindow)
+
     sys.exit(app.exec_())
